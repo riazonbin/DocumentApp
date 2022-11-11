89db706b16e502fb92a1f00492a09352fc2a3d79
riazonbin <riazonbin@mail.ru>
2022-11-11 08:00:43 +0300

    changed reg components, added some design for them.

--- DocumentApp/Services/MongoDBConnection.cs
+++ DocumentApp/Services/MongoDBConnection.cs
@@ -5,24 +5,86 @@ namespace DocumentApp.Services
 {
     public class MongoDBConnection
     {
-        public User? currentUser;
 
-        public void AddToDataBase(User user)
+
+        #region AddToDb
+        public void AddToDataBase(Customer customer)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("DocumentApp");
-            var collection = database.GetCollection<User>("UserCollection");
-            collection.InsertOne(user);
+            var collection = database.GetCollection<Customer>("CustomerCollection");
+            collection.InsertOne(customer);
         }
 
-        public User FindByLogin(string login)
+        public void AddToDataBase(Projecter projecter)
         {
             var client = new MongoClient("mongodb://localhost");
-            var filter = Builders<User>.Filter.Eq("Login", login);
             var database = client.GetDatabase("DocumentApp");
-            var collection = database.GetCollection<User>("UserCollection");
+            var collection = database.GetCollection<Projecter>("ProjecterCollection");
+            collection.InsertOne(projecter);
+        }
+
+        public void AddToDataBase(Developer developer)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("DocumentApp");
+            var collection = database.GetCollection<Developer>("DeveloperCollection");
+            collection.InsertOne(developer);
+        }
+
+        #endregion
+
+
+        #region FindInDb
+        public User? FindByLogin(string login)
+        {
+            var log1 = FindCustomerByLogin(login);
+            var log2 = FindDeveloperByLogin(login);
+            var log3 = FindProjecterByLogin(login);
+
+            if(log1 != null)
+            {
+                return log1;
+            }
+            if (log2 != null)
+            {
+                return log2;
+            }
+            if (log3 != null)
+            {
+                return log3;
+            }
+            return null;
+        }
+
+        public Customer FindCustomerByLogin(string login)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var filter = Builders<Customer>.Filter.Eq("Login", login);
+            var database = client.GetDatabase("DocumentApp");
+            var collection = database.GetCollection<Customer>("CustomerCollection");
             return collection.Find(filter).FirstOrDefault();
         }
+
+        public Projecter FindProjecterByLogin(string login)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var filter = Builders<Projecter>.Filter.Eq("Login", login);
+            var database = client.GetDatabase("DocumentApp");
+            var collection = database.GetCollection<Projecter>("ProjecterCollection");
+            return collection.Find(filter).FirstOrDefault();
+        }
+
+        public Developer FindDeveloperByLogin(string login)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var filter = Builders<Developer>.Filter.Eq("Login", login);
+            var database = client.GetDatabase("DocumentApp");
+            var collection = database.GetCollection<Developer>("DeveloperCollection");
+            return collection.Find(filter).FirstOrDefault();
+        }
+
+        #endregion
 
 
         public IMongoCollection<User> GetCollection()
