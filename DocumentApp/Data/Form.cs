73f00498ff6bfd9532473db4a3f82c0f8559a023
riazonbin <riazonbin@mail.ru>
2022-11-14 12:18:15 +0300

    little changes

--- DocumentApp/Data/Form.cs
+++ DocumentApp/Data/Form.cs
@@ -5,8 +5,15 @@ namespace DocumentApp.Data
 {
     public class Form
     {
+        public Form()
+        {
+            IsApproved = false;
+        }
+
         [BsonId]
         public ObjectId Id { get; set; }
         public List<FormField> FormFields { get; set; } = new List<FormField>();
+
+        public bool IsApproved;
     }
 }
